f8ee36eafdffc47c24284f13f1cd4acb00d28bd0
XiaoxiaTao <31962724+XiaoxiaTao@users.noreply.github.com>
2017-10-13 23:44:44 +0800

    Update Exercise_05:Chapter2 problem2.9.py

--- Exercise_05:Chapter2 problem2.9.py
+++ Exercise_05:Chapter2 problem2.9.py
@@ -26,7 +26,7 @@ class cannon:
         return flight_state(next_x, next_y, next_vx, next_vy, current_state.t + self.dt)
 
     def shoot(self):
-        while not(self.cannon_flight_state[-1].y < 0):#while y>0
+        while not(self.cannon_flight_state[-1].y < 0):
             self.cannon_flight_state.append(self.next_state(self.cannon_flight_state[-1]))
         print self.cannon_flight_state[-1].x, self.cannon_flight_state[-1].y, self.cannon_flight_state[-1].vx, self.cannon_flight_state[-1].vy 
         r = - self.cannon_flight_state[-2].y / self.cannon_flight_state[-1].y#r=-yn/yn+1
@@ -46,9 +46,9 @@ class cannon:
         ylabel('y/m')
         #show()
 
-class drag_cannon(cannon):#drag_cannon inherit cannon
+class drag_cannon(cannon):
     def next_state(self, current_state):
-        global g, b2m#b2m is calculated from the isothermal approximation
+        global g, b2m
         v = sqrt(current_state.vx * current_state.vx + current_state.vy * current_state.vy)            
         next_vx = current_state.vx - b2m * v * current_state.vx * self.dt
         next_x = current_state.x + next_vx* self.dt
@@ -79,21 +79,18 @@ class adia_drag_cannon(cannon):
         next_y = current_state.y + next_vy* self .dt
         return flight_state(next_x, next_y, next_vx, next_vy, current_state.t +self.dt)   
 
-        pass#pass is a null operation, just to make the program complete
-a1 = adia_drag_cannon(flight_state(0,0,573.4,401.2,0),_dt=0.1)#vy/vx=tan35
-a2 = adia_drag_cannon(flight_state(0,0,536.2,449.9,0),_dt=0.1)#vy/vx=tan40
-a3 = adia_drag_cannon(flight_state(0,0,494.9,494.9,0),_dt=0.1)#vy/vx=tan45
-a4 = adia_drag_cannon(flight_state(0,0,449.9,536.2,0),_dt=0.1)#vy/vx=tan50
-a5 = cannon(flight_state(0,0,401.5,573.4,0),_dt=0.1)#vy/vx=tan55
-a1.shoot()#the landing point
+        pass
+a1 = adia_drag_cannon(flight_state(0,0,573.4,401.2,0),_dt=0.1)
+a2 = adia_drag_cannon(flight_state(0,0,536.2,449.9,0),_dt=0.1)
+a3 = adia_drag_cannon(flight_state(0,0,494.9,494.9,0),_dt=0.1)
+a4 = adia_drag_cannon(flight_state(0,0,449.9,536.2,0),_dt=0.1)
+a5 = cannon(flight_state(0,0,401.5,573.4,0),_dt=0.1)
+a1.shoot()
 a2.shoot()
 a3.shoot()
 a4.shoot()
 a5.shoot()
 
-##b = drag_cannon(flight_state(0, 0, 700, 700, 0), _dt = 0.1)
-##b.shoot()
-##b.show_trajectory()
 a1.show_trajectory()
 a2.show_trajectory()
 a3.show_trajectory()
